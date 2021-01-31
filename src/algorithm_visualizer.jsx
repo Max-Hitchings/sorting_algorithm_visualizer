839332b8fe221d8516a6bca0a94a010e85a07c19
gk41 <max.hitchings@gmail.com>
2021-01-31 20:24:58 +0000

    Added an anit spam function to reduce lag on regenerate array

--- src/algorithm_visualizer.jsx
+++ src/algorithm_visualizer.jsx
@@ -1,49 +1,63 @@
 import React, { useRef, useState } from "react";
 import CreateArray from "./CreateArray.jsx";
 import "./CreateArray.css";
-import PropTypes from "prop-types";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import { PrettoSlider } from "@material-ui/core/Slider";
 import Slider from "@material-ui/core/Slider";
-import Typography from "@material-ui/core/Typography";
-import Tooltip from "@material-ui/core/Tooltip";
 
 export default function AlgorithmVisualizer() {
   const [currentArray, setcurrentArray] = useState([]);
   const [arrayLength, setarrayLength] = React.useState(30);
-  const [value, setValue] = React.useState(30);
+  const [sliderTime, setsliderTime] = useState(false);
 
   function GenerateArray() {
     //const Arraylen = ArrayLengthRef.current.value;
-    const Arraylen = arrayLength;
+    if (sliderTime) {
+      const Arraylen = arrayLength;
 
-    let numArray = [];
-    for (let i = 0; i < Arraylen; i++) {
-      numArray = [...numArray, Math.floor(Math.random() * Arraylen) + 1];
+      let numArray = [];
+      for (let i = 0; i < Arraylen; i++) {
+        numArray = [...numArray, Math.floor(Math.random() * Arraylen) + 1];
+      }
+      console.log(numArray);
+      setcurrentArray(numArray);
+      console.log("currentArray", currentArray);
+      if (sliderTime) {
+        setsliderTime(false);
+      }
+      return numArray;
     }
-    console.log(numArray);
-    setcurrentArray(numArray);
-    console.log("currentArray", currentArray);
-    return numArray;
+  }
+
+  async function antiSliderSpam() {
+    setTimeout(() => {
+      console.log("World!");
+      setsliderTime(true);
+      console.log(sliderTime);
+    }, 200);
   }
 
   const handleSliderChange = (event, newValue) => {
     setarrayLength(newValue);
     GenerateArray();
     console.log(newValue);
+    antiSliderSpam();
+  };
+
+  const handleRegenerate = () => {
+    GenerateArray();
+    antiSliderSpam();
   };
 
   const ArrayLengthRef = useRef();
   const slider = useRef();
   return (
     <div>
-      <button onClick={GenerateArray}>Regenerate Current Array</button>
+      <button onClick={handleRegenerate}>Regenerate Current Array</button>
       <Slider
         value={arrayLength}
         onChange={handleSliderChange}
         aria-labelledby="continuous-slider"
         min={5}
-        max={89}
+        max={85}
       />
       <div className="arrayContainer">
         <CreateArray currentArray={[...currentArray]} />
